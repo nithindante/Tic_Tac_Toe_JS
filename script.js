b2a39f87fa9299a6c69c696d2e3ed926c7ec9ad4
nithindante <Rajkumar_nithin@yahoo.com>
2024-06-13 01:57:08 +0530

    One can enter input and check winners and draw

--- script.js
+++ script.js
@@ -7,27 +7,47 @@ Player1 = new Players('Nithin','X')
 Player2 = new Players('Rahul','O')
 
 
+let firstForm = (function () {
+    let mainForm = document.createElement('form')
+    mainForm.classList.add('inputDiv')
+    let firstLabel = document.createElement('label')
+    firstLabel.setAttribute('for','Player1')
+    firstLabel.innerHTML="First Players name";
+    let firstInput = document.createElement('input')
+    firstInput.setAttribute('type','text')
+    firstInput.setAttribute('id','Player1')
+    let secondLabel = document.createElement('label')   
+    secondLabel.setAttribute('for','Player2')
+    secondLabel.innerHTML = "Second player's name";
+    let secondInput = document.createElement('input')
+    secondInput.setAttribute('type','text');
+    secondInput.setAttribute('id','Player2');
+    let button = document.createElement('input');
+    button.setAttribute('type','submit');
+    button.classList.add('submitButton')
+    document.body.appendChild(mainForm)
+    mainForm.appendChild(firstLabel)
+    mainForm.appendChild(firstInput)
+    mainForm.appendChild(secondLabel)
+    mainForm.appendChild(secondInput)
+    mainForm.appendChild(button)
+})();
 
 let board = (function () {
-    let arr =[   [[],[],[]],   [[],[],[]],   [[],[],[]]    ]
-
-
-
-
-
+    let arr =[   [],[],[],   [],[],[],   [],[],[]   ]
     return{  arr };
 })();
 
- // 77476
 
 let displayController = (function() {
-    let createBoard = function(){
         let container = document.createElement('div')
         container.classList.add('container')
         document.body.appendChild(container)
         let board_grid = document.createElement('div')
         board_grid.classList.add('mainGrid')
         container.appendChild(board_grid)
+        
+    let createBoard = function(){
         for (let i = 0; i < 9; i++) {
             let grids = document.createElement('div')
             grids.classList.add('grid')
@@ -36,11 +56,16 @@ let displayController = (function() {
         return
     };
 
-    let placeBoard = function (){
-
-         
+    let placeBoard = function (pos,assignMarker)
+    {
+        game.place(assignMarker,pos)
+        let newDiv = document.createElement('div')
+        newDiv.classList.add('newDiv')
+        childGrids[pos].appendChild(newDiv)
+        newDiv.textContent = assignMarker
     }
-        return {createBoard,placeBoard
+
+        return {createBoard,placeMarker,placeBoard
         };
 
 })();
@@ -48,34 +73,21 @@ let displayController = (function() {
 function Game()
 {
     place = function(marker,pos){
-    board.arr[pos[0]][pos[1]]=marker;
-    return marker,pos;
+    board.arr[pos]=marker;
+    return board.arr
     }
 
-    checkPos = function(pos){
-        console.log(board.arr[pos[0]][pos[1]])
-            if ((board.arr[pos[0]][pos[1]]=='O')||(board.arr[pos[0]][pos[1]]=='X'))
-                {
-                    return false
-                }
-            else{
-                return true;
-            }
-    }
     checkDraw = function(){
         let sum =0 
-        console.log(board.arr)
-        for (let i = 0; i < board.arr.length; i++) {
-            for (let j = 0; j < board.arr[i].length; j++) {
-                if(board.arr[i][j].includes('X'))     
+        for (let i = 0; i < board.arr.length; i++) {  
+                if(board.arr[i].includes('X'))     
                 {
                    sum = sum +1
                 }
-                if (board.arr[i][j].includes('O'))
+                if (board.arr[i].includes('O'))
                 {
                        sum = sum+1
                 }
-            }
         }
         if(sum === 9){
             return true
@@ -85,55 +97,104 @@ function Game()
         }
     }
 
-    checkWin = function(){
-        if ((board.arr[0][0] ==board.arr[0][1] ) && (board.arr[0][1] ==board.arr[0][2]))
+    checkWin = function(){  
+        if ((board.arr[0].toString() ===board.arr[1].toString()) && (board.arr[1].toString() ===board.arr[2].toString()) && (board.arr[1].length>0))
             {
+                
                 return true;
             }
-        else if((board.arr[0][2] ==board.arr[1][2] ) && (board.arr[1][2] ==board.arr[2][2]))
+        else if((board.arr[0].toString() ===board.arr[3].toString() ) && (board.arr[3].toString() ===board.arr[6].toString())&& (board.arr[3].length>0))
             {
-                true;
+               
+                return true;
             }
-        else if ((board.arr[0][1] ==board.arr[1][1] ) && (board.arr[1][1] ==board.arr[2][1]))
+        else if ((board.arr[3].toString() ==board.arr[4].toString() ) && (board.arr[4].toString() ==board.arr[5].toString())&& (board.arr[4].length>0))
             {
+              
               return  true
             }
-        else if ((board.arr[1][0] ==board.arr[1][1] ) && (board.arr[1][1] ==board.arr[1][2]))
+        else if ((board.arr[6].toString() ==board.arr[7].toString() ) && (board.arr[7].toString() ==board.arr[8].toString())&& (board.arr[7].length>0))
             {
+                
                return true
             }
-        else if ((board.arr[2][0] ==board.arr[2][1] ) && (board.arr[2][1] ==board.arr[2][2]))
+        else if ((board.arr[1].toString() ==board.arr[4].toString() ) && (board.arr[4].toString() ==board.arr[7].toString())&& (board.arr[4].length>0))
             {
+                
               return  true
             }
-            else if ((board.arr[0][0] ==board.arr[1][0] ) && (board.arr[1][0] ==board.arr[2][0]))
+            else if ((board.arr[3].toString() ==board.arr[5].toString() ) && (board.arr[5].toString() ==board.arr[8].toString())&& (board.arr[5].length>0))
                 {
+                    
                  return  true
                 }
-                else if ((board.arr[0][2] ==board.arr[1][1] ) && (board.arr[1][1] ==board.arr[2][0]))
+                else if ((board.arr[0].toString() ==board.arr[4].toString() ) && (board.arr[4].toString() ==board.arr[8].toString())&& (board.arr[4].length>0))
                     {
+                       
                       return  true
                     }
-                else if ((board.arr[0][0] ==board.arr[1][1] ) && (board.arr[1][1] ==board.arr[2][2]))
+                else if ((board.arr[2].toString() ==board.arr[4].toString() ) && (board.arr[4].toString() ==board.arr[6].toString())&& (board.arr[2].length>0))
                     {
+                        
+                           
                         return  true
                     }
-                else{
-                    return false
-                }
+                return false
     }
 
    
 
     return{
-        place,checkWin,checkDraw,checkPos
+        place,checkWin,checkDraw
     }
 }
 
+
+
+
+
 let game = new Game()
-console.log(game)
-console.log(board.arr)
-console.log(displayController.createBoard())
-// console.log(game.checkWin())
-// console.log(game.checkPos([2,2])) 
-// console.log(game.checkDraw()) 
+displayController.createBoard()
+
+let mainGrid =document.querySelector('.mainGrid')
+let childGrids = mainGrid.children;
+let assignMarker = 0 
+
+function placeMarker(childGrids,assignMarker) {
+    for (let i = 0; i < childGrids.length; i++) {
+        childGrids[i].addEventListener('click',function () {
+            if (childGrids[i].textContent.trim().length==0)
+            {
+            if (assignMarker == 0) {
+                displayController.placeBoard(i,'O');
+                if(game.checkWin())
+                    {   
+                        console.log('player 1 won')
+                        return
+                    }
+                assignMarker = 1;
+            }
+            else{
+                displayController.placeBoard(i,'X'); 
+                if(game.checkWin())
+                    {   
+                        console.log('player 2 won')
+                        return
+                    }
+                assignMarker = 0;
+            }
+            if (game.checkDraw()) {
+                console.log('Draw')
+                                return
+            }
+        }
+        } )
+
+    }
+}
+placeMarker(childGrids,assignMarker)
+
+let submitButton = document.querySelector('.submitButton');
+submitButton.addEventListener(click(),function () {
+    submitButton.style.display = none;  
+})
